fa2d4d73617a7b5c9a88aedce2a31c4a90240f04
jasonlz <liuzhao44@qq.com>
2015-09-12 15:13:15 +0800

    Tutorial 2

---
+++ Tutorial_2/Hello_dot.cc
@@ -0,0 +1,24 @@
+#include <GL/glew.h>
+#include "math_3d.h"
+
+int main()
+{
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+	    fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+	    return 1;
+	}
+	Vector3f Vertices[1];
+	Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_POINTS, 0, 1);
+	glDisableVertexAttribArray(0);
+	return 0;
+}
